7927afa075fb3377259a64c76d5cee0fe77bbb82
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-10-02 10:42:09 +0500

    Services

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import Portfolio from "../images/portfolio_website";
-import Static from "../images/static_website";
-import Dynamic from "../images/dynamic_website";
+import Portfolio from "../images/portfolio_website.svg";
+import Static from "../images/static_website.svg";
+import Dynamic from "../images/dynamic_website.svg";
 
 const Services = () => {
   return (
-    <div className="container">
+    
+    <div className="container services">
+      <h1 >My Services</h1>
       <div className="row">
         <div className="col col-lg-4 col-md-6 col-sm-12 col-12">
-          <div className="card">
+          <div className="services_card">
             <div className="card__pic">
-              <img src={Portfolio} alt="React Logo" width="100%" />
+              <img src={Portfolio} alt="React Logo" width="140px" />
             </div>
             <div className="card__body">
                 <h2>Portfolio Websites</h2>
@@ -18,19 +20,19 @@ const Services = () => {
           </div>
         </div>
         <div className="col col-lg-4 col-md-6 col-sm-12 col-12">
-          <div className="card">
+          <div className="services_card">
             <div className="card__pic">
-              <img src={Static} alt="React Logo" />
+              <img src={Static} alt="React Logo" width="140px"/>
             </div>
-            <div className="card__body">
+            <div className="card__body pt-3">
                 <h2>Personal Static Websites</h2>
             </div>
           </div>
         </div>
         <div className="col col-lg-4 col-md-6 col-sm-12 col-12">
-          <div className="card">
+          <div className="services_card">
             <div className="card__pic">
-              <img src={Dynamic} alt="React Logo" />
+              <img src={Dynamic} alt="React Logo" width="140px" />
             </div>
             <div className="card__body">
                 <h2>Buisness Dynamic Websites</h2>
